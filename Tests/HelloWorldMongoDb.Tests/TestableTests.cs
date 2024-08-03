8645517119a20783c9245da69be01cf9443bfae8
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-03 01:03:02 +0100

    Add basic project

---
+++ Tests/HelloWorldMongoDb.Tests/TestableTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace HelloWorldMongoDb.Tests;
+
+public class TestableTests
+{
+    [Fact]
+    public void TestableMethod_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var testable = new Testable();
+
+        // Act
+        var result = testable.TestableMethod();
+
+        // Assert
+        result.Should().BeTrue();
+    }
+}
